c9e33f2a195e4803fbecba425486dbae051984e9
Vidhi Saxena <vidhi094btmae23@igdtuw.ac.in>
2024-06-11 00:52:51 +0530

    added axios and api key

--- src/script.js
+++ src/script.js
@@ -1,8 +1,18 @@
+function displayTemperature(response){
+  let tempElement = document.querySelector(".current-temperature");
+  let temp= Math.round(response.)
+}
+
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#input-search").value;
   let cityElement = document.querySelector(".city");
   cityElement.innerHTML = searchInputElement;
+  let city = document.querySelector("#input-search").value;
+  let apikey = "ebcc3971d2aee717f81151084b09afd4 ";
+  let apiurl = `api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=${apikey}`;
+
+    axios.get(apiUrl).then(displayTemperature);
 }
 
 let forms = document.querySelector(".search-form");
